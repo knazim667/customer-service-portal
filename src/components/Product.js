8a9f83d7861dc14e1704c84a7252728098fb13c5
knazim667 <nazamzahoor@yahoo.com>
2022-08-28 17:27:51 -0400

    product and rating initial component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Dell from '../../public/images/dell.webp';
 
 function Product(props) {
     return (
-        <div>
-            
+        <div className='card card-width'>
+            <img src={Dell} alt=""/>
+            <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/" class="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
     );
 }
---
+++ src/components/Rater.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Rater.css';
+
+function Rater(props) {
+    return (
+        <ul className="rating">
+            <li className="filled">{'\u2605'}</li>
+            <li className="filled">{'\u2605'}</li>
+            <li className="filled">{'\u2605'}</li>
+            <li>{'\u2605'}</li>
+            <li>{'\u2605'}</li>
+        </ul>
+    );
+}
+
+export default Rater;
+
